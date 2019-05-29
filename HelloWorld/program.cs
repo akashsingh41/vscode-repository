cb2e9ac984a576d0f580d8c9a149ff131cd6e8df
Singh <akashsingh41@hotmail.com>
2019-05-29 20:04:00 +0530

    adding hello world project

---
+++ HelloWorld/program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Sir/Madam?");
+            //string name = Console.ReadLine();
+            Console.WriteLine("Hello World - Visual Studio Code - BEEEEEEP!!!!");
+            Console.Beep(1000,1000);
+            
+        }
+    }
+}       
+
