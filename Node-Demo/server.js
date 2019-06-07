82c7d42f60f259a6c31d9d239bac4f694b2fe6a6
Singh <akashsingh41@hotmail.com>
2019-06-07 14:35:56 +0530

    read-write stream/file/json/tml

--- Node-Demo/server.js
+++ Node-Demo/server.js
@@ -1,11 +1,31 @@
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function(request, response)
-{
+ var server = http.createServer(function(request, response)
+ {
     console.log('request made @ '+request.url);
-    response.writeHead(200, {'content-type' : 'text/plain'});
-    response.end('heyyy users!!!');
+    response.writeHead(200, {'content-type' : 'application/json'});
+    var myobject = {
+                        name: 'username',
+                        email: 'admin@emailaccount.com',
+                        age: 29,
+                        gender: 'male',                    
+                   };
+    //var myReadStream = fs.createReadStream(__dirname+'/index.html','utf8');    
+    //myReadStream.pipe(response);    
+
+    response.end(JSON.stringify(myobject));
+
 });
 
 server.listen(3000, '127.0.0.1');
 console.log('yooo....listening at port 3000!!!');
+
+
+
+// myReadStream.on('data', function(chunk)
+// {
+//     console.log('data chunk received:');
+//     myWriteStream.write(chunk);
+// });
+
